2d62e6ec8e1a86a41c858ea8bbdcc9c888fc7631
vikas <vikas.shah@bitrise.io>
2023-01-05 23:07:07 -0600

    GitHelper implementation using shell commands

---
+++ step/git_shell_helper.go
@@ -0,0 +1,79 @@
+package step
+
+import (
+	"fmt"
+	"github.com/bitrise-io/go-utils/v2/command"
+	"strings"
+)
+
+type GitShellHelper struct {
+	cmdFactory command.Factory
+	opts       command.Opts
+}
+
+func NewGitShellHelper(cmdFactory command.Factory, projectPath string) GitShellHelper {
+	opts := command.Opts{
+		Dir: projectPath,
+	}
+	helper := GitShellHelper{
+		cmdFactory: cmdFactory,
+		opts:       opts,
+	}
+
+	return helper
+}
+
+func (g GitShellHelper) GetRootPath() (string, error) {
+	//git rev-parse --show-toplevel
+	cmd := g.cmdFactory.Create("git", []string{"rev-parse", "--show-toplevel"}, &g.opts)
+	rootPath, err := cmd.RunAndReturnTrimmedOutput()
+	if err != nil {
+		return "", fmt.Errorf("failed to get root git path error: %s", err)
+	}
+	return rootPath, nil
+}
+
+func (g GitShellHelper) GetRemoteUrl() (string, error) {
+	//git config --get remote.origin.url
+	cmd := g.cmdFactory.Create("git", []string{"config", "--get", "remote.origin.url"}, &g.opts)
+	remoteURL, err := cmd.RunAndReturnTrimmedOutput()
+	if err != nil {
+		return "", fmt.Errorf("failed to get remote url error: %s", err)
+	}
+	return strings.TrimSuffix(remoteURL, ".git"), nil
+}
+
+func (g GitShellHelper) GetCurrentBranch() (string, error) {
+	// git rev-parse --abbrev-ref HEAD
+	cmd := g.cmdFactory.Create("git", []string{"rev-parse", "--abbrev-ref", "HEAD"}, &g.opts)
+	currentBranch, err := cmd.RunAndReturnTrimmedOutput()
+	if err != nil {
+		return "", fmt.Errorf("failed to get current branch error: %s", err)
+	}
+	return currentBranch, nil
+}
+
+func (g GitShellHelper) GetBranchHash(branch string) (string, error) {
+	// git rev-parse --abbrev-ref HEAD
+	cmd := g.cmdFactory.Create("git", []string{"rev-parse", branch}, &g.opts)
+	currentBranchHash, err := cmd.RunAndReturnTrimmedOutput()
+	if err != nil {
+		return "", fmt.Errorf("failed to get hash of current branch: %s", err)
+	}
+	return currentBranchHash, nil
+}
+
+func (g GitShellHelper) GetCurrentBranchHash() (string, error) {
+	currentBranch, err := g.GetCurrentBranch()
+	if err != nil {
+		return "", err
+	}
+
+	var hash string
+	hash, err = g.GetBranchHash(currentBranch)
+	if err != nil {
+		return "", err
+	}
+	
+	return hash, nil
+}
